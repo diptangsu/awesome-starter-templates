7c7e5d56ec4c69b7fd10fda261e888c73137c801
diptangsu <diptangsu.97@gmail.com>
2020-10-01 22:50:27 +0530

    added parser args

--- Python/Flask/basic-CRUD-api/resources/users.py
+++ Python/Flask/basic-CRUD-api/resources/users.py
@@ -4,47 +4,117 @@ from models.users import User
 
 
 class UserResource(Resource):
-    parser = reqparse.RequestParser()
-    parser.add_argument(
+    post_parser = reqparse.RequestParser()
+    post_parser.add_argument(
         'username',
         type=str,
         required=True,
         help="This is a required field"
     )
-    parser.add_argument(
+    post_parser.add_argument(
         'name',
         type=str,
         required=True,
         help="This is a required field"
     )
+    post_parser.add_argument(
+        'age',
+        type=int,
+        required=False,
+    )
+
+    put_parser = reqparse.RequestParser()
+    put_parser.add_argument(
+        'name',
+        type=str,
+    )
+    put_parser.add_argument(
+        'age',
+        type=int,
+    )
 
     def get(self, user_id=None):
-        '''GET /users/{id} --> get user with id=id
         '''
-        if not user_id:
-            abort(404)
-        return User.get(id=user_id)
+        GET /users --> get all users
+        GET /users/{id} --> get user with id=id
+        '''
+        if user_id:
+            return User.get(id=user_id).to_json()
+        else:
+            return [
+                user.to_json()
+                for user in User.get()
+            ]
 
     def post(self, user_id=None):
-        '''POST /users/ --> add new user
+        '''POST /users --> add new user
+
+        Fields:
+            username: username of the user, must be unique
+            name: name of the user
+            age: age of the user
+
+        Returns:
+            Created user object
         '''
         if user_id:
             abort(404)
 
-        data = UserResource.parser.parse_args()
+        data = UserResource.post_parser.parse_args()
 
         username = data.get('username')
-        password = data.get('password')
+        name = data.get('name')
+        age = data.get('age')
 
-        if username and password:
-            user = User(username, password)
+        if username and name:
+            user = User(username, name, age)
+            try:
+                user.save()
+            except:
+                return {
+                    'message': ('Failed to create a new user. '
+                                'Please try another username')
+                }, 424
+
+            return {
+                'message': 'A new user has been created',
+                'user': user.to_json()
+            }, 201
+        else:
+            return {
+                'message': ('Failed to create a new user. '
+                            'All fields are required')
+            }, 424
+
+    def put(self, user_id=None):
+        '''
+        PUT /users/{id} --> update user with id=id
+
+        Fields:
+            name: name of the user
+            age: age of the user
+
+        Returns:
+            Updated user object
+        '''
+        if user_id:
+            user = User.get(id=user_id)
+
+            data = UserResource.put_parser.parse_args()
+
+            name = data.get('name')
+            age = data.get('age')
+
+            if name:
+                user.name = name
+            if age:
+                user.age = age
+
             user.save()
 
             return {
-                'message': 'A new user has been created'
+                'message': 'User object has been updated',
+                'user': user.to_json()
             }
         else:
-            return {
-                'message': 'Failed to create a new user. \
-                            All fields are required'
-            }
+            abort(404)
